cc2a95862578d738e7cbf5ba53ea6deb65f7c556
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-01-07 22:14:24 -0300

    rm executables and change build system

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-clang -o fluids $1 -fsanitize=address -pedantic -Wall -I/opt/hombrew/Cellar/sdl2/2.30.11/include -L/opt/homebrew/Cellar/sdl2/2.30.11/lib -lSDL2
+clang $1 -fsanitize=address -pedantic -Wall -I/opt/hombrew/Cellar/sdl2/2.30.11/include -L/opt/homebrew/Cellar/sdl2/2.30.11/lib -lSDL2
 
 
